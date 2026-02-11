73903a9574728ab3432a53751f58b990ed23e267
equiroz1995 <elizabethgaete1995@gmail.com>
2026-02-11 17:16:03 -0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,4 +1,4 @@
-terraform {
+/* terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
@@ -7,4 +7,22 @@ terraform {
       version = ">= 3.80.0"
     }
   }
+}
+*/
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # o fija la versión que uses en tu org
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id            = var.subscriptionid
+  tenant_id                  = var.tenantid
+  skip_provider_registration = true
+
+
+  features {}
 }
